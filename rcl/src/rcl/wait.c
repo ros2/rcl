f6ba4e3813606a47f6dae12a98a27abca9f6c327
Jackie Kay <jackie@osrfoundation.org>
2016-03-25 16:19:40 -0700

    Extraneous whitespace

--- rcl/src/rcl/wait.c
+++ rcl/src/rcl/wait.c
@@ -146,7 +146,6 @@ rcl_wait_set_init(
   wait_set->impl->rmw_waitset = rmw_create_waitset(
     2 * number_of_subscriptions + number_of_guard_conditions + number_of_clients +
     number_of_services);
-
   if (!wait_set->impl->rmw_waitset) {
     goto fail;
   }
