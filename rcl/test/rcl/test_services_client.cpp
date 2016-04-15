42d82ae7af9e85644014dab5f9100783d5168b94
William Woodall <william@osrfoundation.org>
2016-04-15 14:53:51 -0700

    fix error reporting in test fixtures

--- rcl/test/rcl/test_services_client.cpp
+++ rcl/test/rcl/test_services_client.cpp
@@ -16,10 +16,7 @@
 #include <string>
 #include <thread>
 
-#include "rmw/types.h"
-
 #include "rcl/client.h"
-
 #include "rcl/rcl.h"
 
 #include "example_interfaces/srv/add_two_ints.h"
@@ -43,9 +40,8 @@ wait_for_client_to_be_ready(
   auto wait_set_exit = make_scope_exit([&wait_set]() {
     if (rcl_wait_set_fini(&wait_set) != RCL_RET_OK) {
       fprintf(stderr, "Error in wait set fini: %s\n", rcl_get_error_string_safe());
-      return -1;
+      throw std::runtime_error("error while waiting for client");
     }
-    return 0;
   });
   size_t iteration = 0;
   do {
@@ -77,79 +73,80 @@ wait_for_client_to_be_ready(
 
 int main(int argc, char ** argv)
 {
-  if (rcl_init(argc, argv, rcl_get_default_allocator()) != RCL_RET_OK) {
-    fprintf(stderr, "Error in rcl init: %s\n", rcl_get_error_string_safe());
-    return -1;
-  }
-  rcl_node_t node = rcl_get_zero_initialized_node();
-  const char * name = "node_name";
-  rcl_node_options_t node_options = rcl_node_get_default_options();
-  if (rcl_node_init(&node, name, &node_options) != RCL_RET_OK) {
-    fprintf(stderr, "Error in node init: %s\n", rcl_get_error_string_safe());
-    return -1;
-  }
-  auto node_exit = make_scope_exit([&node]() {
-    if (rcl_node_fini(&node) != RCL_RET_OK) {
-      fprintf(stderr, "Error in node fini: %s\n", rcl_get_error_string_safe());
+  int main_ret = 0;
+  {
+    if (rcl_init(argc, argv, rcl_get_default_allocator()) != RCL_RET_OK) {
+      fprintf(stderr, "Error in rcl init: %s\n", rcl_get_error_string_safe());
       return -1;
     }
-  });
-
-  const rosidl_service_type_support_t * ts = ROSIDL_GET_TYPE_SUPPORT(
-    example_interfaces, srv, AddTwoInts);
-  const char * topic = "add_two_ints";
-
-  rcl_client_t client = rcl_get_zero_initialized_client();
-  rcl_client_options_t client_options = rcl_client_get_default_options();
-  rcl_ret_t ret = rcl_client_init(&client, &node, ts, topic, &client_options);
-  if (ret != RCL_RET_OK) {
-    fprintf(stderr, "Error in client init: %s\n", rcl_get_error_string_safe());
-    return -1;
-  }
-
-  auto client_exit = make_scope_exit([&client, &node]() {
-    if (rcl_client_fini(&client, &node)) {
-      fprintf(stderr, "Error in client fini: %s\n", rcl_get_error_string_safe());
+    rcl_node_t node = rcl_get_zero_initialized_node();
+    const char * name = "node_name";
+    rcl_node_options_t node_options = rcl_node_get_default_options();
+    if (rcl_node_init(&node, name, &node_options) != RCL_RET_OK) {
+      fprintf(stderr, "Error in node init: %s\n", rcl_get_error_string_safe());
       return -1;
     }
-    return 0;
-  });
+    auto node_exit = make_scope_exit([&main_ret, &node]() {
+      if (rcl_node_fini(&node) != RCL_RET_OK) {
+        fprintf(stderr, "Error in node fini: %s\n", rcl_get_error_string_safe());
+        main_ret = -1;
+      }
+    });
 
-  // Initialize a request.
-  example_interfaces__srv__AddTwoInts_Request client_request;
-  example_interfaces__srv__AddTwoInts_Request__init(&client_request);
-  client_request.a = 1;
-  client_request.b = 2;
-  int64_t sequence_number;
+    const rosidl_service_type_support_t * ts = ROSIDL_GET_TYPE_SUPPORT(
+      example_interfaces, srv, AddTwoInts);
+    const char * topic = "add_two_ints";
 
-  if (rcl_send_request(&client, &client_request, &sequence_number)) {
-    fprintf(stderr, "Error in send request: %s\n", rcl_get_error_string_safe());
-    return -1;
+    rcl_client_t client = rcl_get_zero_initialized_client();
+    rcl_client_options_t client_options = rcl_client_get_default_options();
+    rcl_ret_t ret = rcl_client_init(&client, &node, ts, topic, &client_options);
+    if (ret != RCL_RET_OK) {
+      fprintf(stderr, "Error in client init: %s\n", rcl_get_error_string_safe());
+      return -1;
+    }
+
+    auto client_exit = make_scope_exit([&client, &main_ret, &node]() {
+      if (rcl_client_fini(&client, &node)) {
+        fprintf(stderr, "Error in client fini: %s\n", rcl_get_error_string_safe());
+        main_ret = -1;
+      }
+    });
+
+    // Initialize a request.
+    example_interfaces__srv__AddTwoInts_Request client_request;
+    example_interfaces__srv__AddTwoInts_Request__init(&client_request);
+    client_request.a = 1;
+    client_request.b = 2;
+    int64_t sequence_number;
+
+    if (rcl_send_request(&client, &client_request, &sequence_number)) {
+      fprintf(stderr, "Error in send request: %s\n", rcl_get_error_string_safe());
+      return -1;
+    }
+
+    if (sequence_number != 1) {
+      fprintf(stderr, "Got invalid sequence number\n");
+      return -1;
+    }
+
+    example_interfaces__srv__AddTwoInts_Request__fini(&client_request);
+
+    // Initialize the response owned by the client and take the response.
+    example_interfaces__srv__AddTwoInts_Response client_response;
+    example_interfaces__srv__AddTwoInts_Response__init(&client_response);
+
+    if (!wait_for_client_to_be_ready(&client, 10, 10000)) {
+      fprintf(stderr, "Client never became ready\n");
+      return -1;
+    }
+    rmw_request_id_t header;
+    if (rcl_take_response(&client, &header, &client_response) != RCL_RET_OK) {
+      fprintf(stderr, "Error in send response: %s\n", rcl_get_error_string_safe());
+      return -1;
+    }
+
+    example_interfaces__srv__AddTwoInts_Response__fini(&client_response);
   }
 
-  if (sequence_number != 1) {
-    fprintf(stderr, "Got invalid sequence number\n");
-    return -1;
-  }
-
-  example_interfaces__srv__AddTwoInts_Request__fini(&client_request);
-
-
-  // Initialize the response owned by the client and take the response.
-  example_interfaces__srv__AddTwoInts_Response client_response;
-  example_interfaces__srv__AddTwoInts_Response__init(&client_response);
-
-  if (!wait_for_client_to_be_ready(&client, 10, 10000)) {
-    fprintf(stderr, "Client never became ready\n");
-    return -1;
-  }
-  rmw_request_id_t header;
-  if (rcl_take_response(&client, &header, &client_response) != RCL_RET_OK) {
-    fprintf(stderr, "Error in send response: %s\n", rcl_get_error_string_safe());
-    return -1;
-  }
-
-  example_interfaces__srv__AddTwoInts_Response__fini(&client_response);
-
-  return 0;
+  return main_ret;
 }
