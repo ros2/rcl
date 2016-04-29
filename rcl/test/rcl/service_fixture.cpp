d178f6a14922c4dc6a50618c381d06deee828162
Jackie Kay <jackie@osrfoundation.org>
2016-04-29 15:47:40 -0700

    remove extraneous sleep

--- rcl/test/rcl/service_fixture.cpp
+++ rcl/test/rcl/service_fixture.cpp
@@ -113,8 +113,6 @@ int main(int argc, char ** argv)
       }
     });
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-
     // Initialize a response.
     example_interfaces__srv__AddTwoInts_Response service_response;
     example_interfaces__srv__AddTwoInts_Response__init(&service_response);
