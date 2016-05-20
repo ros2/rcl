be755ed2cb7f6ca1a220cabf02fe5531114c9492
William Woodall <william@osrfoundation.org>
2016-05-20 14:53:46 -0700

    tests for graph API

---
+++ rcl/test/rcl/test_graph.cpp
@@ -0,0 +1,188 @@
+// Copyright 2016 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gtest/gtest.h>
+
+#include <algorithm>
+#include <chrono>
+#include <string>
+
+#include "rcl/rcl.h"
+#include "rcl/graph.h"
+
+#include "std_msgs/msg/string.h"
+#include "example_interfaces/srv/add_two_ints.h"
+
+#include "../memory_tools/memory_tools.hpp"
+#include "../scope_exit.hpp"
+#include "rcl/error_handling.h"
+
+#ifdef RMW_IMPLEMENTATION
+# define CLASSNAME_(NAME, SUFFIX) NAME ## __ ## SUFFIX
+# define CLASSNAME(NAME, SUFFIX) CLASSNAME_(NAME, SUFFIX)
+#else
+# define CLASSNAME(NAME, SUFFIX) NAME
+#endif
+
+class CLASSNAME (TestGraphFixture, RMW_IMPLEMENTATION) : public ::testing::Test
+{
+public:
+  rcl_node_t * node_ptr;
+  void SetUp()
+  {
+    stop_memory_checking();
+    rcl_ret_t ret;
+    ret = rcl_init(0, nullptr, rcl_get_default_allocator());
+    ASSERT_EQ(RCL_RET_OK, ret) << rcl_get_error_string_safe();
+    this->node_ptr = new rcl_node_t;
+    *this->node_ptr = rcl_get_zero_initialized_node();
+    const char * name = "node_name";
+    rcl_node_options_t node_options = rcl_node_get_default_options();
+    ret = rcl_node_init(this->node_ptr, name, &node_options);
+    ASSERT_EQ(RCL_RET_OK, ret) << rcl_get_error_string_safe();
+    set_on_unexpected_malloc_callback([]() {ASSERT_FALSE(true) << "UNEXPECTED MALLOC";});
+    set_on_unexpected_realloc_callback([]() {ASSERT_FALSE(true) << "UNEXPECTED REALLOC";});
+    set_on_unexpected_free_callback([]() {ASSERT_FALSE(true) << "UNEXPECTED FREE";});
+    start_memory_checking();
+  }
+
+  void TearDown()
+  {
+    assert_no_malloc_end();
+    assert_no_realloc_end();
+    assert_no_free_end();
+    stop_memory_checking();
+    set_on_unexpected_malloc_callback(nullptr);
+    set_on_unexpected_realloc_callback(nullptr);
+    set_on_unexpected_free_callback(nullptr);
+    rcl_ret_t ret = rcl_node_fini(this->node_ptr);
+    delete this->node_ptr;
+    EXPECT_EQ(RCL_RET_OK, ret) << rcl_get_error_string_safe();
+    ret = rcl_shutdown();
+    EXPECT_EQ(RCL_RET_OK, ret) << rcl_get_error_string_safe();
+  }
+};
+
+/* Test the rcl_service_server_is_available function.
+ */
+TEST_F(CLASSNAME(TestGraphFixture, RMW_IMPLEMENTATION), test_rcl_service_server_is_available) {
+  stop_memory_checking();
+  rcl_ret_t ret;
+  // Create topic publisher just for fun.
+  rcl_publisher_t pub = rcl_get_zero_initialized_publisher();
+  rcl_publisher_options_t pub_options = rcl_publisher_get_default_options();
+  ret = rcl_publisher_init(
+    &pub, this->node_ptr, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, String), "chatter", &pub_options);
+  // First create a client which will be used to call the function.
+  rcl_client_t client = rcl_get_zero_initialized_client();
+  auto ts = ROSIDL_GET_SRV_TYPE_SUPPORT(example_interfaces, AddTwoInts);
+  const char * service_name = "service_test_rcl_service_server_is_available";
+  rcl_client_options_t client_options = rcl_client_get_default_options();
+  ret = rcl_client_init(&client, this->node_ptr, ts, service_name, &client_options);
+  ASSERT_EQ(RCL_RET_OK, ret) << rcl_get_error_string_safe();
+  auto client_exit = make_scope_exit([&client, this]() {
+    stop_memory_checking();
+    rcl_ret_t ret = rcl_client_fini(&client, this->node_ptr);
+    EXPECT_EQ(RCL_RET_OK, ret) << rcl_get_error_string_safe();
+  });
+  // Check, knowing there is no service server (created by us at least).
+  bool is_available;
+  ret = rcl_service_server_is_available(this->node_ptr, &client, &is_available);
+  ASSERT_EQ(RCL_RET_OK, ret) << rcl_get_error_string_safe();
+  ASSERT_FALSE(is_available);
+  // Create the service server.
+  rcl_service_t service = rcl_get_zero_initialized_service();
+  rcl_service_options_t service_options = rcl_service_get_default_options();
+  ret = rcl_service_init(&service, this->node_ptr, ts, service_name, &service_options);
+  ASSERT_EQ(RCL_RET_OK, ret) << rcl_get_error_string_safe();
+  auto service_exit = make_scope_exit([&service, this]() {
+    stop_memory_checking();
+    rcl_ret_t ret = rcl_service_fini(&service, this->node_ptr);
+    EXPECT_EQ(RCL_RET_OK, ret) << rcl_get_error_string_safe();
+  });
+  // Wait for the graph state to change, check, and try again for a period of time.
+  is_available = false;
+  rcl_wait_set_t wait_set = rcl_get_zero_initialized_wait_set();
+  ret = rcl_wait_set_init(&wait_set, 0, 1, 0, 0, 0, rcl_get_default_allocator());
+  ASSERT_EQ(RCL_RET_OK, ret) << rcl_get_error_string_safe();
+  auto wait_set_exit = make_scope_exit([&wait_set, this]() {
+    stop_memory_checking();
+    rcl_ret_t ret = rcl_wait_set_fini(&wait_set);
+    EXPECT_EQ(RCL_RET_OK, ret) << rcl_get_error_string_safe();
+  });
+  const rcl_guard_condition_t * graph_guard_condition =
+    rcl_node_get_graph_guard_condition(this->node_ptr);
+  ASSERT_NE(nullptr, graph_guard_condition) << rcl_get_error_string_safe();
+  auto start = std::chrono::steady_clock::now();
+  auto end = start + std::chrono::seconds(10);
+  while (std::chrono::steady_clock::now() < end) {
+    // We wait multiple times in case other graph changes are occurring simultaneously.
+    std::chrono::nanoseconds time_left = end - start;
+    std::chrono::nanoseconds time_to_sleep = time_left;
+    std::chrono::seconds min_sleep(1);
+    if (time_to_sleep > min_sleep) {
+      time_to_sleep = min_sleep;
+    }
+    ret = rcl_wait_set_clear_guard_conditions(&wait_set);
+    ASSERT_EQ(RCL_RET_OK, ret) << rcl_get_error_string_safe();
+    ret = rcl_wait_set_add_guard_condition(&wait_set, graph_guard_condition);
+    ASSERT_EQ(RCL_RET_OK, ret) << rcl_get_error_string_safe();
+    printf("waiting up to %lld nanoseconds for graph changes\n", time_to_sleep.count());
+    ret = rcl_wait(&wait_set, time_to_sleep.count());
+    if (ret == RCL_RET_TIMEOUT) {
+      printf("timeout!\n");
+      // continue;
+    }
+    else {
+      printf("graph changed!\n");
+    }
+    {
+      rcl_topic_names_and_types_t tnat {};
+      ret = rcl_get_topic_names_and_types(this->node_ptr, &tnat);
+      ASSERT_EQ(ret, RCL_RET_OK) << rcl_get_error_string_safe();
+      printf("Topics:\n");
+      for (size_t i = 0; i < tnat.topic_count; i++) {
+        // if (tnat.topic_names[i][0] != 's') {
+        //   continue;
+        // }
+        printf("  %s:\n", tnat.topic_names[i]);
+        {
+          printf("    type: %s\n", tnat.type_names[i]);
+          size_t count = 0;
+          ret = rcl_count_publishers(this->node_ptr, tnat.topic_names[i], &count);
+          ASSERT_EQ(ret, RCL_RET_OK) << rcl_get_error_string_safe();
+          printf("    publishers: %zu\n", count);
+        }
+        {
+          size_t count = 0;
+          ret = rcl_count_subscribers(this->node_ptr, tnat.topic_names[i], &count);
+          ASSERT_EQ(ret, RCL_RET_OK) << rcl_get_error_string_safe();
+          printf("    subscribers: %zu\n", count);
+        }
+      }
+      ret = rcl_destroy_topic_names_and_types(&tnat);
+      ASSERT_EQ(ret, RCL_RET_OK) << rcl_get_error_string_safe();
+    }
+    if (ret == RCL_RET_TIMEOUT) {
+      continue;
+    }
+    ASSERT_EQ(RCL_RET_OK, ret) << rcl_get_error_string_safe();
+    ret = rcl_service_server_is_available(this->node_ptr, &client, &is_available);
+    ASSERT_EQ(RCL_RET_OK, ret) << rcl_get_error_string_safe();
+    if (is_available) {
+      break;
+    }
+  }
+  ASSERT_TRUE(is_available);
+}
