110aa7a80d8ceb6f62416a25170b33b20f197ee8
William Woodall <william@osrfoundation.org>
2016-05-28 15:12:31 -0700

    condition pragma on not _WIN32

--- rcl/test/rcl/test_graph.cpp
+++ rcl/test/rcl/test_graph.cpp
@@ -15,7 +15,9 @@
 // Disable -Wmissing-field-initializers, as it is overly strict and will be
 // relaxed in future versions of GCC (it is not a warning for clang).
 // See: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=36750#c13
-#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+#ifndef _WIN32
+# pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+#endif
 
 #include <gtest/gtest.h>
 
