69588eff37c720ca207cc4f494da830f9505bcbf
Mikael Arguedas <mikael.arguedas@gmail.com>
2016-10-26 16:41:33 -0700

    sanity check in get_topic_names_and_types

--- rcl/src/rcl/graph.c
+++ rcl/src/rcl/graph.c
@@ -37,6 +37,18 @@ rcl_get_topic_names_and_types(
     return RCL_RET_NODE_INVALID;
   }
   RCL_CHECK_ARGUMENT_FOR_NULL(topic_names_and_types, RCL_RET_INVALID_ARGUMENT);
+  if (topic_names_and_types->topic_count != 0) {
+    RCL_SET_ERROR_MSG("topic count is not zero");
+    return RCL_RET_INVALID_ARGUMENT;
+  }
+  if (topic_names_and_types->topic_names) {
+    RCL_SET_ERROR_MSG("topic names is not null");
+    return RCL_RET_INVALID_ARGUMENT;
+  }
+  if (topic_names_and_types->type_names) {
+    RCL_SET_ERROR_MSG("type names is not null");
+    return RCL_RET_INVALID_ARGUMENT;
+  }
   return rmw_get_topic_names_and_types(
     rcl_node_get_rmw_handle(node),
     topic_names_and_types
