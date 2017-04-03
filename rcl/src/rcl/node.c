12adcba6c837f8906193de1be6c0e1916da5899b
Mikael Arguedas <mikael@osrfoundation.org>
2017-04-03 16:36:56 -0700

    print debug msg to stdout

--- rcl/src/rcl/node.c
+++ rcl/src/rcl/node.c
@@ -208,13 +208,13 @@ rcl_node_init(
   // File discovery magic here
   const char * node_secure_root = rcl_get_secure_root(name);
   if (node_secure_root) {
-    fprintf(stderr,
+    printf(
       "attempting to start a secure node using certificate and key in %s\n",
       node_secure_root);
     node->impl->rmw_node_handle = rmw_create_secure_node(
       name, domain_id, node_secure_root);
   } else {
-    fprintf(stderr,
+    printf(
       "not using security: certificate and key path not found for node %s\n",
       name);
     node->impl->rmw_node_handle = rmw_create_node(name, domain_id);
