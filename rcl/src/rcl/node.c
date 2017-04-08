558b6e592249870b0f40ba2b00428203d0886000
William Woodall <william@osrfoundation.org>
2017-04-07 18:27:44 -0700

    use _snprintf_s on windows

--- rcl/src/rcl/node.c
+++ rcl/src/rcl/node.c
@@ -35,7 +35,8 @@ extern "C"
 #ifndef _WIN32
   #define LOCAL_SNPRINTF snprintf
 #else
-  #define LOCAL_SNPRINTF _snprintf
+  #define LOCAL_SNPRINTF(buffer, buffer_size, format, ...) \
+    _snprintf_s(buffer, buffer_size, _TRUNCATE, format, __VA_ARGS__)
 #endif
 
 typedef struct rcl_node_impl_t
