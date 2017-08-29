312cfecbbdbc7d482591cac73f03207fee1f9aa5
Mikael Arguedas <mikael@osrfoundation.org>
2017-08-29 16:37:16 -0700

    remove todo that doesnt seem necessary anymore

--- rcl/test/rcl/test_publisher.cpp
+++ rcl/test/rcl/test_publisher.cpp
@@ -127,9 +127,7 @@ TEST_F(CLASSNAME(TestPublisherFixture, RMW_IMPLEMENTATION), test_publisher_nomin
   std_msgs__msg__String__init(&msg);
   ASSERT_TRUE(rosidl_generator_c__String__assign(&msg.data, "testing"));
   ret = rcl_publish(&publisher, &msg);
-  // TODO(wjwwood): re-enable this fini when ownership of the string is resolved.
-  //                currently with Connext we will spuriously get a double free here.
-  // std_msgs__msg__String__fini(&msg);
+  std_msgs__msg__String__fini(&msg);
   ASSERT_EQ(RCL_RET_OK, ret) << rcl_get_error_string_safe();
 }
 
