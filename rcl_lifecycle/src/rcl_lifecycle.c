97e25eaac82786960353bb474199e385dd869ddf
dhood <dhood@users.noreply.github.com>
2017-11-17 14:16:42 -0800

    rcl_lifecycle too

--- rcl_lifecycle/src/rcl_lifecycle.c
+++ rcl_lifecycle/src/rcl_lifecycle.c
@@ -152,7 +152,7 @@ rcl_lifecycle_is_valid_transition(
     }
   }
   RCUTILS_LOG_WARN_NAMED(
-    "rcl_lifecycle",
+    ROS_PACKAGE_NAME,
     "No callback transition matching %d found for current state %s",
     key, state_machine->current_state->label)
   return NULL;
@@ -169,7 +169,7 @@ rcl_lifecycle_trigger_transition(
   // If we have a faulty transition pointer
   if (!transition) {
     RCUTILS_LOG_ERROR_NAMED(
-      "rcl_lifecycle",
+      ROS_PACKAGE_NAME,
       "No transition found for node %s with key %d",
       state_machine->current_state->label, key)
     RCL_SET_ERROR_MSG("Transition is not registered.", rcl_get_default_allocator());
@@ -178,7 +178,7 @@ rcl_lifecycle_trigger_transition(
 
   if (!transition->goal) {
     RCUTILS_LOG_ERROR_NAMED(
-      "rcl_lifecycle", "No valid goal is set")
+      ROS_PACKAGE_NAME, "No valid goal is set")
   }
   state_machine->current_state = transition->goal;
   if (publish_notification) {
@@ -200,7 +200,7 @@ rcl_print_state_machine(const rcl_lifecycle_state_machine_t * state_machine)
   const rcl_lifecycle_transition_map_t * map = &state_machine->transition_map;
   for (size_t i = 0; i < map->states_size; ++i) {
     RCUTILS_LOG_INFO_NAMED(
-      "rcl_lifecycle",
+      ROS_PACKAGE_NAME,
       "Primary State: %s(%u)\n# of valid transitions: %u",
       map->states[i].label, map->states[i].id,
       map->states[i].valid_transition_size
