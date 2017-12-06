f0975396d90a13c9cfd8f4019fab1edffb0ca156
dhood <dhood@users.noreply.github.com>
2017-12-05 22:07:35 -0800

    Fail if RMW_IMPLEMENTATION is set but doesn't match (#198)
    * If RMW_IMPLEMENTATION is set, it must match
    rmw_get_implementation_identifier
    
    * Refactor so easier to read
    
    * Add tests
    
    * Use strdup from rcutils
    
    * Don't assume retrieved rmw id is not null
    
    * Add comment clarifying why a check is needed
    
    * Fix long line in test
    
    * Remove test that wouldn't work with multiple rmw impls
    
    * warning -> error
    
    * Check if test exists before setting properties
    
    * Copyright year
    
    * only build executable once
    
    * hopefully fix tests for windows
    
    * Check return code of rcl_init
    
    * Remove another test that wasn't supposed to pass w/ multiple rmw impls
    
    We aren't explicitly setting RMW_IMPLEMENTATION in the cmake for this test (unlike the others)
    
    * Skip tests on windows because of https://github.com/ros2/launch/issues/66

--- rcl/src/rcl/rmw_implementation_identifier_check.c
+++ rcl/src/rcl/rmw_implementation_identifier_check.c
@@ -21,8 +21,10 @@ extern "C"
 #include <stdlib.h>
 #include <string.h>
 
+#include "rcl/allocator.h"
 #include "rcl/error_handling.h"
 #include "rcutils/logging_macros.h"
+#include "rcutils/strdup.h"
 #include "rmw/rmw.h"
 
 #include "rcl/types.h"
@@ -51,10 +53,33 @@ extern "C"
 #endif
 
 INITIALIZER(initialize) {
-  // If the environement variable RCL_ASSERT_RMW_ID_MATCHES is set,
+  // If the environment variable RMW_IMPLEMENTATION is set, or
+  // the environment variable RCL_ASSERT_RMW_ID_MATCHES is set,
   // check that the result of `rmw_get_implementation_identifier` matches.
-  const char * expected = NULL;
-  rcl_ret_t ret = rcl_impl_getenv("RCL_ASSERT_RMW_ID_MATCHES", &expected);
+  rcl_allocator_t allocator = rcl_get_default_allocator();
+  char * expected_rmw_impl = NULL;
+  const char * expected_rmw_impl_env = NULL;
+  rcl_ret_t ret = rcl_impl_getenv("RMW_IMPLEMENTATION", &expected_rmw_impl_env);
+  if (ret != RCL_RET_OK) {
+    RCUTILS_LOG_ERROR_NAMED(
+      ROS_PACKAGE_NAME,
+      "Error getting environement variable 'RMW_IMPLEMENTATION': %s",
+      rcl_get_error_string_safe()
+    )
+    exit(ret);
+  }
+  if (strlen(expected_rmw_impl_env) > 0) {
+    // Copy the environment variable so it doesn't get over-written by the next getenv call.
+    expected_rmw_impl = rcutils_strdup(expected_rmw_impl_env, allocator);
+    if (!expected_rmw_impl) {
+      RCUTILS_LOG_ERROR_NAMED(ROS_PACKAGE_NAME, "allocation failed")
+      exit(RCL_RET_BAD_ALLOC);
+    }
+  }
+
+  char * asserted_rmw_impl = NULL;
+  const char * asserted_rmw_impl_env = NULL;
+  ret = rcl_impl_getenv("RCL_ASSERT_RMW_ID_MATCHES", &asserted_rmw_impl_env);
   if (ret != RCL_RET_OK) {
     RCUTILS_LOG_ERROR_NAMED(
       ROS_PACKAGE_NAME,
@@ -63,16 +88,62 @@ INITIALIZER(initialize) {
     )
     exit(ret);
   }
-  // If the environment variable is set, and it does not match, print a warning and exit.
-  if (strlen(expected) > 0 && strcmp(rmw_get_implementation_identifier(), expected) != 0) {
+  if (strlen(asserted_rmw_impl_env) > 0) {
+    // Copy the environment variable so it doesn't get over-written by the next getenv call.
+    asserted_rmw_impl = rcutils_strdup(asserted_rmw_impl_env, allocator);
+    if (!asserted_rmw_impl) {
+      RCUTILS_LOG_ERROR_NAMED(ROS_PACKAGE_NAME, "allocation failed")
+      exit(RCL_RET_BAD_ALLOC);
+    }
+  }
+
+  // If both environment variables are set, and they do not match, print an error and exit.
+  if (expected_rmw_impl && asserted_rmw_impl && strcmp(expected_rmw_impl, asserted_rmw_impl) != 0) {
     RCUTILS_LOG_ERROR_NAMED(
       ROS_PACKAGE_NAME,
-      "Expected RMW implementation identifier of '%s' but instead found '%s', exiting with %d.",
-      expected,
-      rmw_get_implementation_identifier(),
-      RCL_RET_MISMATCHED_RMW_ID
+      "Values of RMW_IMPLEMENTATION ('%s') and RCL_ASSERT_RMW_ID_MATCHES ('%s') environment "
+      "variables do not match, exiting with %d.",
+      expected_rmw_impl, asserted_rmw_impl, RCL_RET_ERROR
     )
-    exit(RCL_RET_MISMATCHED_RMW_ID);
+    exit(RCL_RET_ERROR);
+  }
+
+  // Collapse the expected_rmw_impl and asserted_rmw_impl variables so only expected_rmw_impl needs
+  // to be used from now on.
+  if (expected_rmw_impl && asserted_rmw_impl) {
+    // The strings at this point must be equal.
+    // No need for asserted_rmw_impl anymore, free the memory.
+    allocator.deallocate((char *)asserted_rmw_impl, allocator.state);
+  } else {
+    // One or none are set.
+    // If asserted_rmw_impl has contents, move it over to expected_rmw_impl.
+    if (asserted_rmw_impl) {
+      expected_rmw_impl = asserted_rmw_impl;
+    }
+  }
+
+  // If either environment variable is set, and it does not match, print an error and exit.
+  if (expected_rmw_impl) {
+    const char * actual_rmw_impl_id = rmw_get_implementation_identifier();
+    if (!actual_rmw_impl_id) {
+      RCUTILS_LOG_ERROR_NAMED(
+        ROS_PACKAGE_NAME,
+        "Error getting RMW implementation identifier."
+      )
+      exit(RCL_RET_ERROR);
+    }
+    if (strcmp(actual_rmw_impl_id, expected_rmw_impl) != 0) {
+      RCUTILS_LOG_ERROR_NAMED(
+        ROS_PACKAGE_NAME,
+        "Expected RMW implementation identifier of '%s' but instead found '%s', exiting with %d.",
+        expected_rmw_impl,
+        actual_rmw_impl_id,
+        RCL_RET_MISMATCHED_RMW_ID
+      )
+      exit(RCL_RET_MISMATCHED_RMW_ID);
+    }
+    // Free the memory now that all checking has passed.
+    allocator.deallocate((char *)expected_rmw_impl, allocator.state);
   }
 }
 
