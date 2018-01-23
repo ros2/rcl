27c3380ab161426e037d72bd28c822448df5c527
jwang <jing.j.wang@intel.com>
2018-01-23 20:40:45 +0800

    setting RMWCount to 0 only when RMWStorage is set to NULL
    Signed-off-by: jwang <jing.j.wang@intel.com>

--- rcl/src/rcl/wait.c
+++ rcl/src/rcl/wait.c
@@ -317,10 +317,10 @@ rcl_wait_set_get_allocator(const rcl_wait_set_t * wait_set, rcl_allocator_t * al
 
 #define SET_RESIZE_RMW_DEALLOC(RMWStorage) \
   /* Also deallocate the rmw storage. */ \
-  wait_set->impl->RMWCount = 0; \
   if (wait_set->impl->RMWStorage) { \
     allocator.deallocate((void *)wait_set->impl->RMWStorage, allocator.state); \
     wait_set->impl->RMWStorage = NULL; \
+    wait_set->impl->RMWCount = 0; \
   }
 
 #define SET_RESIZE_RMW_REALLOC(Type, RMWStorage, RMWCount) \
