b705376910fdfa136dea6fc760958605dc47de6f
Michael Carroll <michael@openrobotics.org>
2018-03-22 15:09:14 -0700

    Appease MSVC's literals.

--- rcl/test/rcl/test_time.cpp
+++ rcl/test/rcl/test_time.cpp
@@ -287,8 +287,8 @@ TEST(CLASSNAME(rcl_time, RMW_IMPLEMENTATION), rcl_time_difference_signed) {
   EXPECT_EQ(retval, RCL_RET_OK) << rcl_get_error_string_safe();
 
   rcl_time_point_t a, b;
-  a.nanoseconds = RCL_S_TO_NS(0l) + 0l;
-  b.nanoseconds = RCL_S_TO_NS(10l) + 0l;
+  a.nanoseconds = RCL_S_TO_NS(0LL) + 0LL;
+  b.nanoseconds = RCL_S_TO_NS(10LL) + 0LL;
   a.clock_type = RCL_ROS_TIME;
   b.clock_type = RCL_ROS_TIME;
 
@@ -297,7 +297,7 @@ TEST(CLASSNAME(rcl_time, RMW_IMPLEMENTATION), rcl_time_difference_signed) {
     rcl_ret_t ret;
     ret = rcl_difference_times(&a, &b, &d);
     EXPECT_EQ(ret, RCL_RET_OK) << rcl_get_error_string_safe();
-    EXPECT_EQ(d.nanoseconds, RCL_S_TO_NS(10l));
+    EXPECT_EQ(d.nanoseconds, RCL_S_TO_NS(10LL));
   }
 
   {
@@ -305,19 +305,19 @@ TEST(CLASSNAME(rcl_time, RMW_IMPLEMENTATION), rcl_time_difference_signed) {
     rcl_ret_t ret;
     ret = rcl_difference_times(&b, &a, &d);
     EXPECT_EQ(ret, RCL_RET_OK) << rcl_get_error_string_safe();
-    EXPECT_EQ(d.nanoseconds, RCL_S_TO_NS(-10l));
+    EXPECT_EQ(d.nanoseconds, RCL_S_TO_NS(-10LL));
   }
 
   // Construct example from issue.
-  a.nanoseconds = RCL_S_TO_NS(1514423496l) + 0l;
-  b.nanoseconds = RCL_S_TO_NS(1514423498l) + 147483647l;
+  a.nanoseconds = RCL_S_TO_NS(1514423496LL) + 0LL;
+  b.nanoseconds = RCL_S_TO_NS(1514423498LL) + 147483647LL;
 
   {
     rcl_duration_t d;
     rcl_ret_t ret;
     ret = rcl_difference_times(&a, &b, &d);
     EXPECT_EQ(ret, RCL_RET_OK) << rcl_get_error_string_safe();
-    EXPECT_EQ(d.nanoseconds, 2147483647l);
+    EXPECT_EQ(d.nanoseconds, 2147483647LL);
   }
 
   {
@@ -325,8 +325,8 @@ TEST(CLASSNAME(rcl_time, RMW_IMPLEMENTATION), rcl_time_difference_signed) {
     rcl_ret_t ret;
     ret = rcl_difference_times(&b, &a, &d);
     EXPECT_EQ(ret, RCL_RET_OK) << rcl_get_error_string_safe();
-    // The erroneous value was -2147483648
-    EXPECT_EQ(d.nanoseconds, -2147483647l);
+    // The erroneous value was -2147483648 (https://github.com/ros2/rcl/issues/204)
+    EXPECT_EQ(d.nanoseconds, -2147483647LL);
   }
 }
 
