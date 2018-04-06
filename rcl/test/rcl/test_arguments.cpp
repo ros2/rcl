9a750fb578751d9df69f4cf52efb84383aace557
Michael Carroll <michael@openrobotics.org>
2018-04-06 09:46:42 -0500

    Fix style issues.

--- rcl/test/rcl/test_arguments.cpp
+++ rcl/test/rcl/test_arguments.cpp
@@ -174,7 +174,7 @@ TEST_F(CLASSNAME(TestArgumentsFixture, RMW_IMPLEMENTATION), test_uninitialized_p
   int argc = sizeof(argv) / sizeof(const char *);
   rcl_arguments_t parsed_args;
   int not_null = 1;
-  parsed_args.impl = reinterpret_cast<rcl_arguments_impl_t*>(&not_null);
+  parsed_args.impl = reinterpret_cast<rcl_arguments_impl_t *>(&not_null);
   ASSERT_EQ(RCL_RET_INVALID_ARGUMENT,
     rcl_parse_arguments(argc, argv, rcl_get_default_allocator(), &parsed_args));
   rcl_reset_error();
@@ -190,9 +190,6 @@ TEST_F(CLASSNAME(TestArgumentsFixture, RMW_IMPLEMENTATION), test_double_parse) {
     rcl_parse_arguments(argc, argv, rcl_get_default_allocator(), &parsed_args));
   rcl_reset_error();
 }
-
-
-
 
 
 TEST_F(CLASSNAME(TestArgumentsFixture, RMW_IMPLEMENTATION), test_fini_null) {
