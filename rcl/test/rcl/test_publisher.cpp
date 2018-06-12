f4fffc71e721ef7dea6d5d93b2e8d761afd6df39
dhood <dhood@users.noreply.github.com>
2018-06-12 08:26:56 -0700

    Use osrf_testing_tools_cpp

--- rcl/test/rcl/test_publisher.cpp
+++ rcl/test/rcl/test_publisher.cpp
@@ -122,7 +122,6 @@ TEST_F(CLASSNAME(TestPublisherFixture, RMW_IMPLEMENTATION), test_publisher_nomin
  * different message types, because at the time partitions were used for implementing namespaces.
  */
 TEST_F(CLASSNAME(TestPublisherFixture, RMW_IMPLEMENTATION), test_publishers_different_types) {
-  stop_memory_checking();
   rcl_ret_t ret;
   rcl_publisher_t publisher = rcl_get_zero_initialized_publisher();
   const rosidl_message_type_support_t * ts_int = ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int64);
@@ -131,12 +130,10 @@ TEST_F(CLASSNAME(TestPublisherFixture, RMW_IMPLEMENTATION), test_publishers_diff
   rcl_publisher_options_t publisher_options = rcl_publisher_get_default_options();
   ret = rcl_publisher_init(&publisher, this->node_ptr, ts_int, topic_name, &publisher_options);
   ASSERT_EQ(RCL_RET_OK, ret) << rcl_get_error_string_safe();
-  auto publisher_exit = make_scope_exit(
-    [&publisher, this]() {
-      stop_memory_checking();
-      rcl_ret_t ret = rcl_publisher_fini(&publisher, this->node_ptr);
-      EXPECT_EQ(RCL_RET_OK, ret) << rcl_get_error_string_safe();
-    });
+  OSRF_TESTING_TOOLS_CPP_SCOPE_EXIT({
+    rcl_ret_t ret = rcl_publisher_fini(&publisher, this->node_ptr);
+    EXPECT_EQ(RCL_RET_OK, ret) << rcl_get_error_string_safe();
+  });
   EXPECT_EQ(strcmp(rcl_publisher_get_topic_name(&publisher), expected_topic_name), 0);
 
   rcl_publisher_t publisher_in_namespace = rcl_get_zero_initialized_publisher();
@@ -147,12 +144,10 @@ TEST_F(CLASSNAME(TestPublisherFixture, RMW_IMPLEMENTATION), test_publishers_diff
   ret = rcl_publisher_init(
     &publisher_in_namespace, this->node_ptr, ts_string, topic_name, &publisher_options);
   ASSERT_EQ(RCL_RET_OK, ret) << rcl_get_error_string_safe();
-  auto publisher_in_namespace_exit = make_scope_exit(
-    [&publisher_in_namespace, this]() {
-      stop_memory_checking();
-      rcl_ret_t ret = rcl_publisher_fini(&publisher_in_namespace, this->node_ptr);
-      EXPECT_EQ(RCL_RET_OK, ret) << rcl_get_error_string_safe();
-    });
+  OSRF_TESTING_TOOLS_CPP_SCOPE_EXIT({
+    rcl_ret_t ret = rcl_publisher_fini(&publisher_in_namespace, this->node_ptr);
+    EXPECT_EQ(RCL_RET_OK, ret) << rcl_get_error_string_safe();
+  });
   EXPECT_EQ(strcmp(rcl_publisher_get_topic_name(&publisher_in_namespace), expected_topic_name), 0);
 
   std_msgs__msg__Int64 msg_int;
