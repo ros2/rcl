f5d9463f5657b636301a47b2add3dd4673a445a7
Jacob Perron <jacob@openrobotics.org>
2018-10-26 09:42:05 -0700

    [rcl_action] Add wait set related functions

--- rcl_action/include/rcl_action/wait.h
+++ rcl_action/include/rcl_action/wait.h
@@ -25,7 +25,7 @@ extern "C"
 #include "rcl/wait.h"
 
 
-/// Add an action client to a wait set.
+/// Add a rcl_action_client_t to a wait set.
 /**
  * This function will add the underlying service clients and subscriber to the wait set.
  *
@@ -57,11 +57,11 @@ extern "C"
 RCL_PUBLIC
 RCL_WARN_UNUSED
 rcl_ret_t
-rcl_wait_set_add_action_client(
+rcl_action_wait_set_add_action_client(
   rcl_wait_set_t * wait_set,
   const rcl_action_client_t * action_client);
 
-/// Add an action server to a wait set.
+/// Add a rcl_action_server_t to a wait set.
 /**
  * This function will add the underlying services to the wait set.
  *
@@ -89,9 +89,166 @@ rcl_wait_set_add_action_client(
 RCL_PUBLIC
 RCL_WARN_UNUSED
 rcl_ret_t
-rcl_wait_set_add_action_server(
+rcl_action_wait_set_add_action_server(
   rcl_wait_set_t * wait_set,
   const rcl_action_server_t * action_server);
+
+/// Get the number of wait set entities associated with a rcl_action_client_t.
+/**
+ * Returns the number of entities that are added to the wait set if
+ * rcl_action_wait_set_add_action_client() is called.
+ *
+ * <hr>
+ * Attribute          | Adherence
+ * ------------------ | -------------
+ * Allocates Memory   | No
+ * Thread-Safe        | No
+ * Uses Atomics       | No
+ * Lock-Free          | Yes
+ *
+ * \param[in] action_client an action client to query
+ * \param[out] num_subscriptions the number of subscriptions added when the action client
+ *   is added to the wait set
+ * \param[out] num_guard_conditions the number of guard conditions added when the action client
+ *   is added to the wait set
+ * \param[out] num_timers the number of timers added when the action client
+ *   is added to the wait set
+ * \param[out] num_clients the number of clients added when the action client
+ *   is added to the wait set
+ * \param[out] num_services the number of services added when the action client
+ *   is added to the wait set
+ * \return `RCL_RET_OK` if call is successful, or
+ * \return `RCL_RET_INVALID_ARGUMENT` if any arguments are invalid, or
+ * \return `RCL_RET_ACTION_CLIENT_INVALID` if the action client is invalid, or
+ * \return `RCL_RET_ERROR` if an unspecified error occurs.
+*/
+RCL_PUBLIC
+RCL_WARN_UNUSED
+rcl_ret_t
+rcl_action_client_wait_set_get_num_entities(
+  const rcl_action_client_t * action_client,
+  size_t * num_subscriptions,
+  size_t * num_guard_conditions,
+  size_t * num_timers,
+  size_t * num_clients,
+  size_t * num_services);
+
+/// Get the number of wait set entities associated with a rcl_action_server_t.
+/**
+ * Returns the number of entities that are added to the wait set if
+ * rcl_action_wait_set_add_action_server() is called.
+ *
+ * <hr>
+ * Attribute          | Adherence
+ * ------------------ | -------------
+ * Allocates Memory   | No
+ * Thread-Safe        | No
+ * Uses Atomics       | No
+ * Lock-Free          | Yes
+ *
+ * \param[in] action_server an action server to query
+ * \param[out] num_subscriptions the number of subscriptions added when the action server
+ *   is added to the wait set
+ * \param[out] num_guard_conditions the number of guard conditions added when the action server
+ *   is added to the wait set
+ * \param[out] num_timers the number of timers added when the action server
+ *   is added to the wait set
+ * \param[out] num_clients the number of clients added when the action server
+ *   is added to the wait set
+ * \param[out] num_services the number of services added when the action server
+ *   is added to the wait set
+ * \return `RCL_RET_OK` if call is successful, or
+ * \return `RCL_RET_INVALID_ARGUMENT` if any arguments are invalid, or
+ * \return `RCL_RET_ACTION_SERVER_INVALID` if the action server is invalid, or
+ * \return `RCL_RET_ERROR` if an unspecified error occurs.
+*/
+RCL_PUBLIC
+RCL_WARN_UNUSED
+rcl_ret_t
+rcl_action_server_wait_set_get_num_entities(
+  const rcl_action_server_t * action_server,
+  size_t * num_subscriptions,
+  size_t * num_guard_conditions,
+  size_t * num_timers,
+  size_t * num_clients,
+  size_t * num_services);
+
+/// Get the wait set entities that are ready for a rcl_action_client_t.
+/**
+ * The caller can use this function to determine the relevant action client functions
+ * to call: rcl_action_take_feedback(), rcl_action_take_status(),
+ * rcl_action_take_goal_response(), rcl_action_take_cancel_response(), or
+ * rcl_action_take_result_response().
+ * <hr>
+ * Attribute          | Adherence
+ * ------------------ | -------------
+ * Allocates Memory   | No
+ * Thread-Safe        | No
+ * Uses Atomics       | No
+ * Lock-Free          | Yes
+ *
+ * \param[in] action_client an action client to query
+ * \param[out] is_feedback_ready `true` if there is a feedback message ready to take,
+ *   `false` otherwise
+ * \param[out] is_status_message `true` if there is a status message ready to take,
+ *   `false` otherwise
+ * \param[out] is_goal_response_ready `true` if there is a goal response message ready
+ *   to take, `false` otherwise
+ * \param[out] is_cancel_response_ready `true` if there is a cancel response message ready
+ *   to take, `false` otherwise
+ * \param[out] is_result_response_ready `true` if there is a result response message ready
+ *   to take, `false` otherwise
+ * \return `RCL_RET_OK` if call is successful, or
+ * \return `RCL_RET_INVALID_ARGUMENT` if any arguments are invalid, or
+ * \return `RCL_RET_ACTION_CLIENT_INVALID` if the action client is invalid, or
+ * \return `RCL_RET_ERROR` if an unspecified error occurs.
+*/
+RCL_PUBLIC
+RCL_WARN_UNUSED
+rcl_ret_t
+rcl_action_client_wait_set_get_entities_ready(
+  const rcl_wait_set_t * wait_set,
+  const rcl_action_client_t * action_client,
+  bool * is_feedback_ready,
+  bool * is_status_ready,
+  bool * is_goal_response_ready,
+  bool * is_cancel_response_ready,
+  bool * is_result_response_ready);
+
+/// Get the wait set entities that are ready for a rcl_action_server_t.
+/**
+ * The caller can use this function to determine the relevant action server functions
+ * to call: rcl_action_take_goal_request(), rcl_action_take_cancel_request(), or
+ * rcl_action_take_result_request().
+ * <hr>
+ * Attribute          | Adherence
+ * ------------------ | -------------
+ * Allocates Memory   | No
+ * Thread-Safe        | No
+ * Uses Atomics       | No
+ * Lock-Free          | Yes
+ *
+ * \param[in] action_server an action server to query
+ * \param[out] is_goal_request_ready `true` if there is a goal request message ready
+ *   to take, `false` otherwise
+ * \param[out] is_cancel_request_ready `true` if there is a cancel request message ready
+ *   to take, `false` otherwise
+ * \param[out] is_result_request_ready `true` if there is a result request message ready
+ *   to take, `false` otherwise
+ * \return `RCL_RET_OK` if call is successful, or
+ * \return `RCL_RET_INVALID_ARGUMENT` if any arguments are invalid, or
+ * \return `RCL_RET_ACTION_CLIENT_INVALID` if the action server is invalid, or
+ * \return `RCL_RET_ERROR` if an unspecified error occurs.
+*/
+RCL_PUBLIC
+RCL_WARN_UNUSED
+rcl_ret_t
+rcl_action_server_wait_set_get_entities_ready(
+  const rcl_wait_set_t * wait_set,
+  const rcl_action_server_t * action_server,
+  bool * is_goal_request_ready,
+  bool * is_cancel_request_ready,
+  bool * is_result_request_ready);
 
 #ifdef __cplusplus
 }
