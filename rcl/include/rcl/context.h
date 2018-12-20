be8e8e8e19d7457c689267488b41c4a0108619bc
Sander van Dijk <sgvandijk@gmail.com>
2018-12-20 08:47:48 +0000

    Ensure that context instance id storage is aligned correctly

--- rcl/include/rcl/context.h
+++ rcl/include/rcl/context.h
@@ -29,6 +29,8 @@ extern "C"
 #include "rcl/types.h"
 #include "rcl/visibility_control.h"
 
+#include <stdalign.h>
+
 typedef uint64_t rcl_context_instance_id_t;
 
 struct rcl_context_impl_t;
@@ -132,7 +134,7 @@ typedef struct rcl_context_t
    * See this paper for an effort to make this possible in the future:
    *   http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2018/p0943r1.html
    */
-  uint8_t instance_id_storage[RCL_CONTEXT_ATOMIC_INSTANCE_ID_STORAGE_SIZE];
+  alignas(uint_least64_t) uint8_t instance_id_storage[RCL_CONTEXT_ATOMIC_INSTANCE_ID_STORAGE_SIZE];
 } rcl_context_t;
 
 /// Return a zero initialization context object.
