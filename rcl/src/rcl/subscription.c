0efd10ea9ced9ebcd024c799dceab074e3a08967
Karsten Knese <karsten@openrobotics.org>
2019-09-30 00:05:49 -0700

    first draft of loaned message sequence
    Signed-off-by: Karsten Knese <karsten@openrobotics.org>

--- rcl/src/rcl/subscription.c
+++ rcl/src/rcl/subscription.c
@@ -310,6 +310,76 @@ rcl_take_serialized_message(
   return RCL_RET_OK;
 }
 
+rcl_ret_t
+rcl_take_loaned_message_sequence(
+  const rcl_subscription_t * subscription,
+  rmw_loaned_message_sequence_t * loaned_message_sequence,
+  rmw_message_info_sequence_t * message_info_sequence,
+  size_t n,
+  rmw_subscription_allocation_t * allocation
+)
+{
+  RCUTILS_LOG_DEBUG_NAMED(ROS_PACKAGE_NAME, "Subscription taking loaned message sequence");
+  if (!rcl_subscription_is_valid(subscription)) {
+    return RCL_RET_SUBSCRIPTION_INVALID;  // error already set
+  }
+  RCL_CHECK_ARGUMENT_FOR_NULL(loaned_message_sequence, RCL_RET_INVALID_ARGUMENT);
+  // If message_info is NULL, use a place holder which can be discarded.
+  rmw_message_info_sequence_t dummy_message_info_sequence;
+  rmw_message_info_sequence_t * message_info_local =
+    message_info_sequence ? message_info_sequence : &dummy_message_info_sequence;
+  // Call rmw_take_with_info.
+  bool taken = false;
+  rmw_ret_t ret = rmw_take_loaned_message_sequence_with_info(
+    subscription->impl->rmw_handle,
+    loaned_message_sequence,
+    n,
+    &taken,
+    message_info_local,
+    allocation);
+  if (ret != RMW_RET_OK) {
+    RCL_SET_ERROR_MSG(rmw_get_error_string().str);
+    if (RMW_RET_BAD_ALLOC == ret) {
+      return RCL_RET_BAD_ALLOC;
+    }
+    return RCL_RET_ERROR;
+  }
+  RCUTILS_LOG_DEBUG_NAMED(
+    ROS_PACKAGE_NAME,
+    "Subscription loaned message sequence take succeeded: %s", taken ? "true" : "false");
+  if (!taken) {
+    return RCL_RET_SUBSCRIPTION_TAKE_FAILED;
+  }
+  return RCL_RET_OK;
+}
+
+rcl_ret_t
+rcl_return_loaned_message_sequence(
+  const rcl_subscription_t * subscription,
+  rmw_loaned_message_sequence_t * loaned_message_sequence
+)
+{
+  RCUTILS_LOG_DEBUG_NAMED(ROS_PACKAGE_NAME, "Subscription taking loaned message sequence");
+  if (!rcl_subscription_is_valid(subscription)) {
+    return RCL_RET_SUBSCRIPTION_INVALID;  // error already set
+  }
+  RCL_CHECK_ARGUMENT_FOR_NULL(loaned_message_sequence, RCL_RET_INVALID_ARGUMENT);
+
+  rmw_ret_t ret = rmw_return_loaned_message_sequence(
+    subscription->impl->rmw_handle, loaned_message_sequence);
+  if (ret != RMW_RET_OK) {
+    RCL_SET_ERROR_MSG(rmw_get_error_string().str);
+    if (RMW_RET_BAD_ALLOC == ret) {
+      return RCL_RET_BAD_ALLOC;
+    }
+    return RCL_RET_ERROR;
+  }
+  RCUTILS_LOG_DEBUG_NAMED(
+    ROS_PACKAGE_NAME,
+    "Subscription loaned message sequence return succeeded");
+  return RCL_RET_OK;
+}
+
 const char *
 rcl_subscription_get_topic_name(const rcl_subscription_t * subscription)
 {
