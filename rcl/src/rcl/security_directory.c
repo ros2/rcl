f6730a02341c9220677c99e005f314b718f9c947
ahcorde <ahcorde@gmail.com>
2020-03-31 10:31:10 +0200

    removed unnecessary rcutils_reset_error in secutiry_directory
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- rcl/src/rcl/security_directory.c
+++ rcl/src/rcl/security_directory.c
@@ -107,7 +107,6 @@ static bool get_best_matching_directory(
     rcutils_ret_t ret = rcutils_file_fini(&file, dir.allocator);
     if (ret != RCUTILS_RET_OK) {
       RCL_SET_ERROR_MSG_WITH_FORMAT_STRING("%s", rcutils_get_error_string().str);
-      rcutils_reset_error();
       goto cleanup;
     }
     if (RCUTILS_RET_OK != rcutils_next_dir(&dir)) {
@@ -118,7 +117,6 @@ static bool get_best_matching_directory(
 cleanup:
   if (rcutils_close_dir(&dir) != RCUTILS_RET_OK) {
     RCL_SET_ERROR_MSG_WITH_FORMAT_STRING("%s", rcutils_get_error_string().str);
-    rcutils_reset_error();
   }
   return max_match_length > 0;
 }
