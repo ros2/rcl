e494eb8217c7b67f5f187b0c5946a2ac57181d41
Dan Rose <dan@digilabs.io>
2020-05-15 15:54:29 -0500

    Consolidate macro duplication
    Signed-off-by: Dan Rose <dan@digilabs.io>

--- rcl/include/rcl/macros.h
+++ rcl/include/rcl/macros.h
@@ -15,22 +15,10 @@
 #ifndef RCL__MACROS_H_
 #define RCL__MACROS_H_
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
+#include "rcutils/macros.h"
 
-#ifndef _WIN32
-/// Ignored return values of functions with this macro will emit a warning.
-# define RCL_WARN_UNUSED __attribute__((warn_unused_result))
-#else
-# define RCL_WARN_UNUSED _Check_return_
-#endif
+#define RCL_WARN_UNUSED RCUTILS_WARN_UNUSED
 
-#define RCL_UNUSED(x) (void)(x)
-
-#ifdef __cplusplus
-}
-#endif
+#define RCL_UNUSED(x) RCUTILS_UNUSED(x)
 
 #endif  // RCL__MACROS_H_
