ec170f2dff72e2e3546888a698a09c40367326ee
Jorge Perez <jjperez@ekumenlabs.com>
2020-06-01 18:22:03 -0300

    Add basic test get_default_domain_id
    Signed-off-by: Jorge Perez <jjperez@ekumenlabs.com>

---
+++ rcl/test/rcl/test_domain_id.cpp
@@ -0,0 +1,28 @@
+// Copyright 2020 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gtest/gtest.h>
+
+#include "rcl/rcl.h"
+#include "rcl/domain_id.h"
+#include "rcutils/env.h"
+
+TEST(TestGetDomainId, test_nominal) {
+  ASSERT_TRUE(rcutils_set_env("ROS_DOMAIN_ID", "42"));
+  size_t domain_id = 0u;
+  EXPECT_EQ(RCL_RET_OK, rcl_get_default_domain_id(&domain_id));
+  EXPECT_EQ(42u, domain_id);
+
+  EXPECT_EQ(RCL_RET_INVALID_ARGUMENT, rcl_get_default_domain_id(nullptr));
+}
