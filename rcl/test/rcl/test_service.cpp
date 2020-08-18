0d37d778d888fdeeca198dd91cd273fa22e8680a
Jorge Perez <jjperez@ekumenlabs.com>
2020-08-18 18:55:17 -0300

    Add fini tests
    Signed-off-by: Jorge Perez <jjperez@ekumenlabs.com>

--- rcl/test/rcl/test_service.cpp
+++ rcl/test/rcl/test_service.cpp
@@ -514,3 +514,27 @@ TEST_F(CLASSNAME(TestServiceFixture, RMW_IMPLEMENTATION), test_fail_ini_mocked) 
     rcl_reset_error();
   }
 }
+
+/* Mock fini failed tests
+ */
+TEST_F(CLASSNAME(TestServiceFixture, RMW_IMPLEMENTATION), test_fail_fini_mocked) {
+  const rosidl_service_type_support_t * ts = ROSIDL_GET_SRV_TYPE_SUPPORT(
+    test_msgs, srv, BasicTypes);
+  const char * topic = "primitives";
+
+  rcl_service_t service = rcl_get_zero_initialized_service();
+  rcl_service_options_t service_options = rcl_service_get_default_options();
+  rcl_ret_t ret = rcl_service_init(&service, this->node_ptr, ts, topic, &service_options);
+  ASSERT_EQ(RCL_RET_OK, ret) << rcl_get_error_string().str;
+
+  rcl_service_t empty_service = rcl_get_zero_initialized_service();
+  ret = rcl_service_fini(&empty_service, this->node_ptr);
+  EXPECT_EQ(RCL_RET_OK, ret) << rcl_get_error_string().str;
+
+  auto mock = mocking_utils::inject_on_return(
+    "lib:rcl", rmw_destroy_service, RMW_RET_ERROR);
+  ret = rcl_service_fini(&service, this->node_ptr);
+  EXPECT_EQ(RCL_RET_ERROR, ret) << rcl_get_error_string().str;
+  EXPECT_TRUE(rcl_error_is_set());
+  rcl_reset_error();
+}
