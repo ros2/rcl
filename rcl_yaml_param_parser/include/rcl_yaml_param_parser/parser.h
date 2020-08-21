56a398b7e3aefa27129d6962e68e10432bf7b9be
Ada-King <Bingtao.Du@sony.com>
2020-08-21 11:06:39 +0800

    Satisfy windows CI
    Signed-off-by: Ada-King <Bingtao.Du@sony.com>

--- rcl_yaml_param_parser/include/rcl_yaml_param_parser/parser.h
+++ rcl_yaml_param_parser/include/rcl_yaml_param_parser/parser.h
@@ -32,6 +32,10 @@
 #include "rcl_yaml_param_parser/types.h"
 #include "rcl_yaml_param_parser/visibility_control.h"
 
+#ifdef _WIN32
+#define strcasecmp stricmp
+#endif
+
 #ifdef __cplusplus
 extern "C"
 {
