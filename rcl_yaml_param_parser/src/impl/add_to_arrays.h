5663f24234bb86eeb6e8d74669350ca7bbb0c3b1
brawner <brawner@gmail.com>
2020-09-01 15:36:24 -0700

    Refactor parser.c for better testability (#754)
    * Refactor rcl_yaml_param_parser for better testability
    
    Signed-off-by: Stephen Brawner <brawner@gmail.com>
    
    * Reorder parser.c to match parser.h
    
    Signed-off-by: Stephen Brawner <brawner@gmail.com>
    
    squash! Reorder parser.c to match parser.h
    
    * Refactor yaml_variant.c for deduplication
    
    Signed-off-by: Stephen Brawner <brawner@gmail.com>
    
    * ADD_VALUE_TO_SIMPLE_ARRAY for deduplication
    
    Signed-off-by: Stephen Brawner <brawner@gmail.com>
    
    * Remove fprintf
    
    Signed-off-by: Stephen Brawner <brawner@gmail.com>
    
    * PR Fixup
    
    Signed-off-by: Stephen Brawner <brawner@gmail.com>
    
    * Move headers to src directory
    
    Signed-off-by: Stephen Brawner <brawner@gmail.com>
    
    * PR Fixup
    
    Signed-off-by: Stephen Brawner <brawner@gmail.com>
    
    * Rebase #780
    
    Signed-off-by: Stephen Brawner <brawner@gmail.com>

---
+++ rcl_yaml_param_parser/src/impl/add_to_arrays.h
@@ -0,0 +1,79 @@
+// Copyright 2018 Apex.AI, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef IMPL__ADD_TO_ARRAYS_H_
+#define IMPL__ADD_TO_ARRAYS_H_
+
+#include <yaml.h>
+
+#include "rcutils/types.h"
+
+#include "./types.h"
+#include "rcl_yaml_param_parser/types.h"
+#include "rcl_yaml_param_parser/visibility_control.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+///
+/// Add a value to a bool array. Create the array if it does not exist
+///
+RCL_YAML_PARAM_PARSER_PUBLIC
+RCUTILS_WARN_UNUSED
+rcutils_ret_t add_val_to_bool_arr(
+  rcl_bool_array_t * const val_array,
+  bool * value,
+  const rcutils_allocator_t allocator);
+
+///
+/// Add a value to an integer array. Create the array if it does not exist
+///
+RCL_YAML_PARAM_PARSER_PUBLIC
+RCUTILS_WARN_UNUSED
+rcutils_ret_t add_val_to_int_arr(
+  rcl_int64_array_t * const val_array,
+  int64_t * value,
+  const rcutils_allocator_t allocator);
+
+///
+/// Add a value to a double array. Create the array if it does not exist
+///
+RCL_YAML_PARAM_PARSER_PUBLIC
+RCUTILS_WARN_UNUSED
+rcutils_ret_t add_val_to_double_arr(
+  rcl_double_array_t * const val_array,
+  double * value,
+  const rcutils_allocator_t allocator);
+
+///
+/// Add a value to a string array. Create the array if it does not exist
+///
+RCL_YAML_PARAM_PARSER_PUBLIC
+RCUTILS_WARN_UNUSED
+rcutils_ret_t add_val_to_string_arr(
+  rcutils_string_array_t * const val_array,
+  char * value,
+  const rcutils_allocator_t allocator);
+
+///
+/// TODO (anup.pemmaiah): Support byte array
+///
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // IMPL__ADD_TO_ARRAYS_H_
---
+++ rcl_yaml_param_parser/src/impl/parse.h
@@ -0,0 +1,94 @@
+// Copyright 2018 Apex.AI, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef IMPL__PARSE_H_
+#define IMPL__PARSE_H_
+
+#include <yaml.h>
+
+#include "rcutils/types.h"
+
+#include "./types.h"
+#include "rcl_yaml_param_parser/types.h"
+#include "rcl_yaml_param_parser/visibility_control.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+RCL_YAML_PARAM_PARSER_PUBLIC
+RCUTILS_WARN_UNUSED
+void * get_value(
+  const char * const value,
+  yaml_scalar_style_t style,
+  data_types_t * val_type,
+  const rcutils_allocator_t allocator);
+
+RCL_YAML_PARAM_PARSER_PUBLIC
+RCUTILS_WARN_UNUSED
+rcutils_ret_t parse_value(
+  const yaml_event_t event,
+  const bool is_seq,
+  const size_t node_idx,
+  const size_t parameter_idx,
+  data_types_t * seq_data_type,
+  rcl_params_t * params_st);
+
+RCL_YAML_PARAM_PARSER_PUBLIC
+RCUTILS_WARN_UNUSED
+rcutils_ret_t parse_key(
+  const yaml_event_t event,
+  uint32_t * map_level,
+  bool * is_new_map,
+  size_t * node_idx,
+  size_t * parameter_idx,
+  namespace_tracker_t * ns_tracker,
+  rcl_params_t * params_st);
+
+RCL_YAML_PARAM_PARSER_PUBLIC
+RCUTILS_WARN_UNUSED
+rcutils_ret_t parse_file_events(
+  yaml_parser_t * parser,
+  namespace_tracker_t * ns_tracker,
+  rcl_params_t * params_st);
+
+RCL_YAML_PARAM_PARSER_PUBLIC
+RCUTILS_WARN_UNUSED
+rcutils_ret_t parse_value_events(
+  yaml_parser_t * parser,
+  const size_t node_idx,
+  const size_t parameter_idx,
+  rcl_params_t * params_st);
+
+RCL_YAML_PARAM_PARSER_PUBLIC
+RCUTILS_WARN_UNUSED
+rcutils_ret_t find_node(
+  const char * node_name,
+  rcl_params_t * param_st,
+  size_t * node_idx);
+
+RCL_YAML_PARAM_PARSER_PUBLIC
+RCUTILS_WARN_UNUSED
+rcutils_ret_t find_parameter(
+  const size_t node_idx,
+  const char * parameter_name,
+  rcl_params_t * param_st,
+  size_t * parameter_idx);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // IMPL__PARSE_H_
