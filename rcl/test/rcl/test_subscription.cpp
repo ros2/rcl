1828e2638744e3106ebfe54782eccfa873310166
Barry Xu <barry.xu@sony.com>
2020-09-07 17:34:29 +0800

    Fix memory leak on serialized message in test_publisher/subscription.cpp
    Signed-off-by: Barry Xu <barry.xu@sony.com>

--- rcl/test/rcl/test_subscription.cpp
+++ rcl/test/rcl/test_subscription.cpp
@@ -571,6 +571,10 @@ TEST_F(CLASSNAME(TestSubscriptionFixture, RMW_IMPLEMENTATION), test_subscription
   {
     ret = rcl_subscription_fini(&subscription, this->node_ptr);
     EXPECT_EQ(RCL_RET_OK, ret) << rcl_get_error_string().str;
+
+    test_msgs__msg__Strings__fini(&msg);
+    ret = rmw_serialized_message_fini(&serialized_msg);
+    ASSERT_EQ(RCUTILS_RET_OK, ret) << rcl_get_error_string().str;
   });
   rcl_reset_error();
 
@@ -595,6 +599,10 @@ TEST_F(CLASSNAME(TestSubscriptionFixture, RMW_IMPLEMENTATION), test_subscription
     ASSERT_EQ(RMW_RET_OK, ret);
     ASSERT_EQ(
       std::string(test_string), std::string(msg_rcv.string_value.data, msg_rcv.string_value.size));
+
+    test_msgs__msg__Strings__fini(&msg_rcv);
+    ret = rmw_serialized_message_fini(&serialized_msg_rcv);
+    ASSERT_EQ(RCL_RET_OK, ret) << rcl_get_error_string().str;
   }
 }
 
