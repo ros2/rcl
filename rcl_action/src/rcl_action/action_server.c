4ff7f9c7f0ae30fcfe7113b29abc65e6856323a4
Stephen Brawner <brawner@gmail.com>
2020-09-09 11:25:33 -0700

    Cleanup
    Signed-off-by: Stephen Brawner <brawner@gmail.com>

--- rcl_action/src/rcl_action/action_server.c
+++ rcl_action/src/rcl_action/action_server.c
@@ -175,8 +175,8 @@ rcl_action_server_init(
 
 // Initialize Timer
   ret = rcl_timer_init(
-    &action_server->impl->expire_timer, &action_server->impl->clock, node->context, options->result_timeout.nanoseconds,
-    NULL, allocator);
+    &action_server->impl->expire_timer, &action_server->impl->clock, node->context,
+    options->result_timeout.nanoseconds, NULL, allocator);
   if (RCL_RET_OK != ret) {
     goto fail;
   }
@@ -185,7 +185,7 @@ rcl_action_server_init(
   if (RCL_RET_OK != ret) {
     goto fail;
   }
-  
+
   // Copy action name
   action_server->impl->action_name = rcutils_strdup(action_name, allocator);
   if (NULL == action_server->impl->action_name) {
