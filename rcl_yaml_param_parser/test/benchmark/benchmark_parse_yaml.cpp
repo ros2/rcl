bcee9ccfe73a0ad82f08f8e5638bc383bb38175f
ahcorde <ahcorde@gmail.com>
2020-09-23 22:56:14 +0200

    Used rcpputils for the path
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- rcl_yaml_param_parser/test/benchmark/benchmark_parse_yaml.cpp
+++ rcl_yaml_param_parser/test/benchmark/benchmark_parse_yaml.cpp
@@ -18,6 +18,8 @@
 
 #include "rcl_yaml_param_parser/parser.h"
 
+#include "rcpputils/filesystem_helper.hpp"
+
 #include "rcutils/allocator.h"
 #include "rcutils/error_handling.h"
 #include "rcutils/filesystem.h"
@@ -30,23 +32,11 @@ public:
   void InitYamlStructure(const std::string & filename, ::benchmark::State & st)
   {
     rcutils_reset_error();
-    allocator = rcutils_get_default_allocator();
-    if (!rcutils_get_cwd(cur_dir, 1024)) {
+    path = rcpputils::fs::current_path().string() + "/test/benchmark/" + filename.c_str();
+    if (path.empty()) {
       st.SkipWithError(rcutils_get_error_string().str);
     }
-    test_path = rcutils_join_path(cur_dir, "test/benchmark", allocator);
-    if (test_path == NULL) {
-      st.SkipWithError(rcutils_get_error_string().str);
-    }
-    path = rcutils_join_path(test_path, filename.c_str(), allocator);
-    if (path == NULL) {
-      st.SkipWithError(rcutils_get_error_string().str);
-    }
-    params_hdl = rcl_yaml_node_struct_init(allocator);
-    if (NULL == params_hdl) {
-      st.SkipWithError(rcutils_get_error_string().str);
-    }
-    params_hdl = rcl_yaml_node_struct_init(allocator);
+    params_hdl = rcl_yaml_node_struct_init(rcutils_get_default_allocator());
     if (NULL == params_hdl) {
       st.SkipWithError(rcutils_get_error_string().str);
     }
@@ -56,15 +46,9 @@ public:
   void TearDown(::benchmark::State & st)
   {
     performance_test_fixture::PerformanceTest::TearDown(st);
-    allocator.deallocate(test_path, allocator.state);
-    allocator.deallocate(path, allocator.state);
     rcl_yaml_node_struct_fini(params_hdl);
   }
-
-  char cur_dir[1024];
-  char * path;
-  char * test_path;
-  rcutils_allocator_t allocator;
+  std::string path;
   rcl_params_t * params_hdl;
 };
 
@@ -72,8 +56,7 @@ BENCHMARK_F(PerformanceTestPaserYaml, parser_yaml_max_params)(benchmark::State &
 {
   InitYamlStructure("max_num_params.yaml", st);
   for (auto _ : st) {
-
-    bool res = rcl_parse_yaml_file(path, params_hdl);
+    bool res = rcl_parse_yaml_file(path.c_str(), params_hdl);
     if (!res) {
       rcl_yaml_node_struct_fini(params_hdl);
       st.SkipWithError(rcutils_get_error_string().str);
