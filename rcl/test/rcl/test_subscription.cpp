ba6f619ad93ef198a106b08d8f939589640e58db
Chen Lihui <Lihui.Chen@sony.com>
2021-02-25 17:07:51 +0800

    update comments to make linelength <= 100
    Signed-off-by: Chen Lihui <Lihui.Chen@sony.com>

--- rcl/test/rcl/test_subscription.cpp
+++ rcl/test/rcl/test_subscription.cpp
@@ -1104,7 +1104,7 @@ TEST_F(CLASSNAME(TestSubscriptionFixtureInit, RMW_IMPLEMENTATION), test_subscrip
   rcl_reset_error();
 }
 
-/* Test for all failure modes in subscription rcl_subscription_set_cft_expression_parameters function.
+/* Test for all failure modes in rcl_subscription_set_cft_expression_parameters function.
  */
 TEST_F(
   CLASSNAME(
@@ -1150,7 +1150,7 @@ TEST_F(
   }
 }
 
-/* Test for all failure modes in subscription rcl_subscription_get_cft_expression_parameters function.
+/* Test for all failure modes in rcl_subscription_get_cft_expression_parameters function.
  */
 TEST_F(
   CLASSNAME(
