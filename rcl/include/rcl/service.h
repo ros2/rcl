78f3829261fb92b1f20e424c7b7304056673cf2f
William Woodall <william@osrfoundation.org>
2022-02-22 21:29:22 -0800

    fix messed up merge
    Signed-off-by: William Woodall <william@osrfoundation.org>

--- rcl/include/rcl/service.h
+++ rcl/include/rcl/service.h
@@ -441,7 +441,6 @@ RCL_PUBLIC
 bool
 rcl_service_is_valid(const rcl_service_t * service);
 
-<<<<<<< HEAD
 /// Get the actual qos settings of the service's request subscription.
 /**
  * Used to get the actual qos settings of the service's request subscription.
@@ -494,12 +493,12 @@ RCL_WARN_UNUSED
 const rmw_qos_profile_t *
 rcl_service_response_publisher_get_actual_qos(const rcl_service_t * service);
 
-/// Set the callback function for the event.
+/// Set the on new request callback function for the service.
 /**
  * This API sets the callback function to be called whenever the
- * event is notified about a new instance of the event.
+ * service is notified about a new request.
  *
- * \sa rmw_event_set_callback for more details about this function.
+ * \sa rmw_service_set_on_new_request_callback for details about this function.
  *
  * <hr>
  * Attribute          | Adherence
@@ -510,18 +509,18 @@ rcl_service_response_publisher_get_actual_qos(const rcl_service_t * service);
  * Lock-Free          | Maybe [1]
  * <i>[1] rmw implementation defined</i>
  *
- * \param[in] event The event on which to set the callback
- * \param[in] callback The callback to be called when new events occur, may be NULL
+ * \param[in] service The service on which to set the callback
+ * \param[in] callback The callback to be called when new requests arrive, may be NULL
  * \param[in] user_data Given to the callback when called later, may be NULL
  * \return `RCL_RET_OK` if callback was set to the listener, or
- * \return `RCL_RET_INVALID_ARGUMENT` if `event` is NULL, or
+ * \return `RCL_RET_INVALID_ARGUMENT` if `service` is NULL, or
  * \return `RCL_RET_UNSUPPORTED` if the API is not implemented in the dds implementation
  */
 RCL_PUBLIC
 RCL_WARN_UNUSED
 rcl_ret_t
-rcl_event_set_callback(
-  const rcl_event_t * event,
+rcl_service_set_on_new_request_callback(
+  const rcl_service_t * service,
   rcl_event_callback_t callback,
   const void * user_data);
 
