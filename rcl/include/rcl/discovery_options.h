4c4248f1b2bad856940a97303843baaba8b1a2ff
William Woodall <william@osrfoundation.org>
2023-03-29 16:09:56 -0700

    fixup docs
    Signed-off-by: William Woodall <william@osrfoundation.org>

--- rcl/include/rcl/discovery_options.h
+++ rcl/include/rcl/discovery_options.h
@@ -31,10 +31,20 @@ extern "C"
 
 /// Determine how the user wishes to discover other ROS nodes automatically.
 /**
- * Checks an environment variable to determine how far automatic discovery should be allowed to
- * propagate: not at all, the local machine only, or however far the automatic discovery mechanism
- * used by the RMW implementation can propagate on the network (e.g. for multicast-based discovery,
- * this will be the local subnet).
+ * Use the ROS_AUTOMATIC_DISCOVERY_RANGE environment variable to determine how
+ * far automatic discovery should be allowed to propagate:
+ *
+ *   - not at all (no automatic discovery),
+ *   - the local machine only,
+ *   - or the subnet (or as far beyond the local system as the middleware can)
+ *
+ * When the subnet is specified the automatic discovery mechanism used by the
+ * rmw implementation dictates how far discovery can propagate on the network,
+ * e.g. for multicast-based discovery this will be the local subnet, hence the
+ * name.
+ *
+ * The option indicated by the environment variable will be stored in the
+ * automatic_discovery_range field of the given discovery_options struct.
  *
  * \param[out] discovery_options Must not be NULL.
  * \return #RCL_RET_INVALID_ARGUMENT if an argument is invalid, or
@@ -56,17 +66,24 @@ rcl_automatic_discovery_range_to_string(rmw_automatic_discovery_range_t automati
 
 /// Determine how the user wishes to discover other ROS nodes via statically-configured peers.
 /**
- * Checks an environment variable to determine the hosts that the user wants to communicate with,
- * in addition to localhost.
+ * Use the ROS_STATIC_PEERS environment variable to determine which hosts
+ * the user wants to communicate with, in addition to localhost.
  *
  * Values for the static peers are not validated beyond basic string checks,
- * avoiding empty strings, etc.
+ * e.g. avoiding empty strings, etc.
  * Any validation of IP addresses or hostnames is left up to the
  * rmw implementation, and therefore what is and is not acceptable in these
  * fields is dependent on it.
  *
  * The general expectation, however, is that IP addresses and hostnames are
  * acceptable.
+ *
+ * The static peers are split by ';' and returned as a list of fixed length
+ * c-strings in the static_peers and static_peers_count fields of the given
+ * discovery_options struct.
+ * Each peer may only be RMW_DISCOVERY_OPTIONS_STATIC_PEERS_MAX_LENGTH long,
+ * and if it is longer it will be skipped and a warning log message will be
+ * produced.
  *
  * \param[out] discovery_options Must not be NULL.
  * \return #RCL_RET_INVALID_ARGUMENT if an argument is invalid, or
