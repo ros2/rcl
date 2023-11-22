dde4730e7116ed1f4075e98781a87b6cf19ecba0
Chris Lalancette <clalancette@gmail.com>
2023-11-22 19:01:52 +0000

    Remove AMENT_DEPENDENCIES from rcl_add_custom_gtest.
    There are no more users of it.
    
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

--- rcl/test/cmake/rcl_add_custom_gtest.cmake
+++ rcl/test/cmake/rcl_add_custom_gtest.cmake
@@ -21,9 +21,8 @@ set(rcl_add_custom_gtest_INCLUDED TRUE)
 # Custom macro for adding a gtest in rcl.
 #
 # It also takes some of the arguments of ament_add_gtest as well as
-# INCLUDE_DIRS, LIBRARIES, and AMENT_DEPENDENCIES which are passed to
-# target_include_directories(), target_link_libraries(), and
-# ament_target_dependencies() respectively.
+# INCLUDE_DIRS and LIBRARIES which are passed to
+# target_include_directories() and target_link_libraries(), respectively.
 #
 # :param target: the target name which will also be used as the test name
 # :type target: string
@@ -41,8 +40,6 @@ set(rcl_add_custom_gtest_INCLUDED TRUE)
 # :type INCLUDE_DIRS: list of strings
 # :param LIBRARIES: list of libraries to link to the target
 # :type LIBRARIES: list of strings
-# :param AMENT_DEPENDENCIES: list of depends to pass ament_target_dependencies
-# :type AMENT_DEPENDENCIES: list of strings
 #
 # @public
 #
@@ -50,7 +47,7 @@ macro(rcl_add_custom_gtest target)
   cmake_parse_arguments(_ARG
     "SKIP_TEST;TRACE"
     "TIMEOUT"
-    "SRCS;ENV;APPEND_ENV;APPEND_LIBRARY_DIRS;INCLUDE_DIRS;LIBRARIES;AMENT_DEPENDENCIES"
+    "SRCS;ENV;APPEND_ENV;APPEND_LIBRARY_DIRS;INCLUDE_DIRS;LIBRARIES"
     ${ARGN})
   if(_ARG_UNPARSED_ARGUMENTS)
     message(FATAL_ERROR "rcl_add_custom_gtest() called with unused arguments: ${_ARG_UNPARSED_ARGUMENTS}")
@@ -94,13 +91,6 @@ macro(rcl_add_custom_gtest target)
         message(STATUS "  rcl_add_custom_gtest() LIBRARIES: ${_ARG_LIBRARIES}")
       endif()
       target_link_libraries(${target} ${_ARG_LIBRARIES})
-    endif()
-    # Add extra ament dependencies, if any.
-    if(_ARG_AMENT_DEPENDENCIES)
-      if(_ARG_TRACE)
-        message(STATUS "  rcl_add_custom_gtest() AMENT_DEPENDENCIES: ${_ARG_AMENT_DEPENDENCIES}")
-      endif()
-      ament_target_dependencies(${target} ${_ARG_AMENT_DEPENDENCIES})
     endif()
     target_compile_definitions(${target}
       PUBLIC "RMW_IMPLEMENTATION=${rmw_implementation}")
