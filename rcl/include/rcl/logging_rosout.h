f9ef0ba9949708b2c6ac4826bd158fbea20cf521
Tomoya Fujita <Tomoya.Fujita@sony.com>
2025-01-22 12:23:22 -0800

    keep rcl_qos_profile_rosout_default with deprecation warning.
    Signed-off-by: Tomoya Fujita <Tomoya.Fujita@sony.com>

--- rcl/include/rcl/logging_rosout.h
+++ rcl/include/rcl/logging_rosout.h
@@ -20,13 +20,25 @@
 #include "rcl/allocator.h"
 #include "rcl/error_handling.h"
 #include "rcl/node.h"
+#include "rcl/macros.h"
 #include "rcl/types.h"
 #include "rcl/visibility_control.h"
+
+#include "rmw/qos_profiles.h"
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
+
+/// The default qos profile setting for topic /rosout
+/**
+ * - depth = 1000
+ * - durability = RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL
+ * - lifespan = {10, 0}
+ */
+RCUTILS_DEPRECATED_WITH_MSG("use rmw_qos_profile_rosout_default instead")
+static const rmw_qos_profile_t rcl_qos_profile_rosout_default = rmw_qos_profile_rosout_default;
 
 /// Initializes the rcl_logging_rosout features
 /**
