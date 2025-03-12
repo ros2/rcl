4cfd6dedd312cc0955a5107ebe2e0e9b21c87e1f
Alejandro Hernández Cordero <ahcorde@gmail.com>
2025-03-12 12:04:39 +0100

    use rmw_event_type_is_supported (#1214)
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>
    (cherry picked from commit ddae02ffeff4f43c7b5f618aced78f7f1c3d9c1f)
    
    # Conflicts:
    #	rcl/test/rcl/test_events.cpp

--- rcl/test/rcl/test_events.cpp
+++ rcl/test/rcl/test_events.cpp
@@ -754,6 +754,13 @@ TEST_F(TestEventFixture, test_event_is_invalid) {
  */
 TEST_F(TestEventFixture, test_sub_message_lost_event)
 {
+<<<<<<< HEAD
+=======
+  if (!rmw_event_type_is_supported(RMW_EVENT_MESSAGE_LOST)) {
+    GTEST_SKIP();
+  }
+
+>>>>>>> ddae02f (use rmw_event_type_is_supported (#1214))
   const rmw_qos_profile_t subscription_qos_profile = default_qos_profile;
 
   rcl_ret_t ret = setup_subscriber(subscription_qos_profile);
