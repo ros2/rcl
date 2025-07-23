f9a58d25e0b38a7150fdec2ee17542abc70586c2
Rahul K.A <karahul209@gmail.com>
2025-07-23 09:59:15 -0700

    chore: Adjust `write_structured_parameter` args
    Signed-off-by: Rahul K.A <karahul209@gmail.com>

--- rcl_yaml_param_parser/src/impl/parse.h
+++ rcl_yaml_param_parser/src/impl/parse.h
@@ -55,7 +55,8 @@ rcutils_ret_t write_structured_parameter_to_string(
   yaml_parser_t * parser,
   yaml_event_t * current_event,
   uint32_t * map_depth,
-  size_t * parameter_index,
+  const size_t node_index,
+  const size_t parameter_index,
   rcl_params_t * params_st);
 
 RCL_YAML_PARAM_PARSER_PUBLIC
