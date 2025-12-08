ea1f3d0d26801277329dc136b07bc37c0b5a724f
Tomoya Fujita <Tomoya.Fujita@sony.com>
2025-12-08 16:54:13 +0900

    1st draft bring-up with rcl_logging_implementation package support.
    Signed-off-by: Tomoya Fujita <Tomoya.Fujita@sony.com>

--- rcl/cmake/get_default_rcl_logging_implementation.cmake
+++ rcl/cmake/get_default_rcl_logging_implementation.cmake
@@ -23,24 +23,35 @@
 #
 macro(get_default_rcl_logging_implementation var)
 
-  # if logging implementation already specified or RCL_LOGGING_IMPLEMENTATION environment variable
-  # is set then use that, otherwise default to using rcl_logging_spdlog
+  # Check if static logging implementation is requested at build time
+  # Priority: CMake variable > Environment variable > Default
   if(NOT "${RCL_LOGGING_IMPLEMENTATION}" STREQUAL "")
     set(_logging_implementation "${RCL_LOGGING_IMPLEMENTATION}")
-  elseif(NOT "$ENV{RCL_LOGGING_IMPLEMENTATION}" STREQUAL "")
+  elseif(DEFINED ENV{RCL_LOGGING_IMPLEMENTATION})
     set(_logging_implementation "$ENV{RCL_LOGGING_IMPLEMENTATION}")
   else()
-    set(_logging_implementation rcl_logging_spdlog)
+    set(_logging_implementation "rcl_logging_implementation")
   endif()
 
-  # persist implementation decision in cache
-  # if it was not determined dynamically
-  set(
-    RCL_LOGGING_IMPLEMENTATION "${_logging_implementation}"
-    CACHE STRING "select rcl logging implementation to use" FORCE
-  )
+  # For static build configuration (opt-in)
+  if(NOT _logging_implementation STREQUAL "rcl_logging_implementation")
+    message(STATUS "Building rcl with static logging implementation: ${_logging_implementation}")
+    find_package(${_logging_implementation} REQUIRED)
+    # Persist implementation decision in cache
+    set(
+      RCL_LOGGING_IMPLEMENTATION "${_logging_implementation}"
+      CACHE STRING "select rcl logging implementation to use" FORCE
+    )
+  else()
+    # Dynamic loading support, so we can choose logging implementation at runtime
+    message(STATUS "Building rcl with dynamic logging implementation support")
+    find_package(rcl_logging_implementation REQUIRED)
+    # Clear the cache to indicate dynamic loading
+    set(
+      RCL_LOGGING_IMPLEMENTATION ""
+      CACHE STRING "select rcl logging implementation to use" FORCE
+    )
+  endif()
 
-  find_package("${_logging_implementation}" REQUIRED)
-
-  set(${var} ${_logging_implementation})
+  set(${var} "${_logging_implementation}")
 endmacro()
